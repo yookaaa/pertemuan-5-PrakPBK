919003b77d6675d5d23a403d452dc8f199f98c59
yookaaa <ekamaulanahidayat@gmail.com>
2025-06-14 08:15:37 +0700

    merubah vite config, main js dan style quasar

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+// FILE: vite.config.js
 
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    vue({
+      template: { transformAssetUrls }
+    }),
+
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      )
+    })
+  ]
 })
