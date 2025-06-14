fa2a2472cc2ad5a1567816e91e8c30551a19798c
yookaaa <ekamaulanahidayat@gmail.com>
2025-06-14 08:36:28 +0700

    membuat pages

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { Quasar } from 'quasar'
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
 
-// Import Quasar css
+
 import 'quasar/src/css/index.sass'
 
 // Assumes your root component is App.vue
---
+++ src/pages/AboutView.vue
@@ -0,0 +1,29 @@
+<script>
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+export default {
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    const search = computed({
+      get() {
+        return route.query.search ?? ''
+      },
+      set(search) {
+        router.replace({ query: { search } })
+      }
+    })
+
+    return { search }
+  }
+}
+</script>
+
+<template>
+  <h2>AboutView</h2>
+  <label>
+    Search: <input v-model.trim="search" maxlength="28">
+  </label>
+</template>
